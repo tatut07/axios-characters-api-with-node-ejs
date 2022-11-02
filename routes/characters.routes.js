69e14e3ec70901559dcfc67c1b97555cc2298aab
Tatiana Toledo <tatiana@tatianas-mbp.speedport.ip>
2022-11-02 17:31:49 +0100

    iteration1

--- routes/characters.routes.js
+++ routes/characters.routes.js
@@ -14,9 +14,28 @@ router.get("/characters", (req, res, next) => {
     .catch((err) => console.error(err));
 });
 
+/*CREATE new character*/
+
 router.get("/characters/create", (req, res, next) => {
   res.render("characters/create-character");
 });
+
+router.post("/characters/create", (req, res, next) => {
+  if (req.body.debt === "on") {
+    req.body.debt = true;
+  } else {
+    req.body.debt = false;
+  }
+  axios
+    .post("https://ih-crud-api.herokuapp.com/characters", req.body)
+    .then((responseFromAPI) => {
+      console.log(responseFromAPI);
+      res.redirect("/characters");
+    })
+    .catch((err) => console.error(err));
+});
+
+/*GET Character ID*/
 
 router.get("/characters/:id", (req, res, next) => {
   axios
@@ -30,17 +49,42 @@ router.get("/characters/:id", (req, res, next) => {
     .catch((err) => console.error(err));
 });
 
-/*CREATE new character*/
-router.post("/characters/create", (req, res, next) => {
+/*Edit character*/
+
+router.get("/characters/:id/edit", (req, res, next) => {
+  axios
+    .get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
+    .then((responseFromAPI) => {
+      res.render("characters/edit-character", {
+        character: responseFromAPI.data,
+      });
+    })
+    .catch((err) => console.error(err));
+});
+
+router.post("/characters/:id/update", (req, res, next) => {
   if (req.body.debt === "on") {
     req.body.debt = true;
   } else {
     req.body.debt = false;
   }
   axios
-    .post("https://ih-crud-api.herokuapp.com/characters", req.body)
+    .put(
+      `https://ih-crud-api.herokuapp.com/characters/${req.params.id}`,
+      req.body
+    )
     .then((responseFromAPI) => {
-      console.log(responseFromAPI);
+      res.redirect("characters/${req.params.id}");
+    })
+    .catch((err) => console.error(err));
+});
+
+/*delete character*/
+
+router.post("/characters/:id/delete", (req, res, next) => {
+  axios
+    .delete(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
+    .then((responseFromAPI) => {
       res.redirect("/characters");
     })
     .catch((err) => console.error(err));
