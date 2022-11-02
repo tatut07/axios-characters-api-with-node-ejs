2129686181923c2b33e497531bbaeb8a2f0c560c
Tatiana Toledo <tatiana@tatianas-mbp.speedport.ip>
2022-11-02 15:25:05 +0100

    iteration1

--- routes/characters.routes.js
+++ routes/characters.routes.js
@@ -3,25 +3,49 @@ const axios = require("axios");
 
 /* GET home page */
 router.get("/characters", (req, res, next) => {
-    axios.get("https://ih-crud-api.herokuapp.com/characters")
-    .then(responseFromAPI => {
-        // console.log(responseFromAPI)
-        res.render("characters/list-characters", { characters: responseFromAPI.data });
+  axios
+    .get("https://ih-crud-api.herokuapp.com/characters")
+    .then((responseFromAPI) => {
+      // console.log(responseFromAPI)
+      res.render("characters/list-characters", {
+        characters: responseFromAPI.data,
+      });
     })
-    .catch(err => console.error(err))
+    .catch((err) => console.error(err));
 });
 
+router.get("/characters/create", (req, res, next) => {
+  res.render("characters/create-character");
+});
 
 router.get("/characters/:id", (req, res, next) => {
-    axios.get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
-    .then(responseFromAPI => {
-        // console.log("details: ", responseFromAPI.data)
-        res.render("characters/details-character", { character: responseFromAPI.data });
+  axios
+    .get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
+    .then((responseFromAPI) => {
+      // console.log("details: ", responseFromAPI.data)
+      res.render("characters/details-character", {
+        character: responseFromAPI.data,
+      });
     })
-    .catch(err => console.error(err))
+    .catch((err) => console.error(err));
+});
+
+/*CREATE new character*/
+router.post("/characters/create", (req, res, next) => {
+  if (req.body.debt === "on") {
+    req.body.debt = true;
+  } else {
+    req.body.debt = false;
+  }
+  axios
+    .post("https://ih-crud-api.herokuapp.com/characters", req.body)
+    .then((responseFromAPI) => {
+      console.log(responseFromAPI);
+      res.redirect("/characters");
+    })
+    .catch((err) => console.error(err));
 });
 
 module.exports = router;
-
 
 // https://ih-crud-api.herokuapp.com/characters
